9e70a4785ec86494f3b2d3c0648f71bd0be00a05
Nick Ruggero <druggero7@gmail.com>
2017-10-22 01:05:31 -0400

    Update after part-3 completion

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,13 +3,10 @@
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
+          <v-toolbar-title>Login</v-toolbar-title>
         </v-toolbar>
 
         <div class="pl-4 pr-4 pt-2 pb-2">
-          <form
-          name="tab-tracker-form"
-          autocomplete="off">
           <v-text-field
               label="Email"
               v-model="email"
@@ -19,17 +16,15 @@
               label="Password"
               type="password"
               v-model="password"
-              autocomplete="new-password"
             ></v-text-field>
-          </form>
           <br>
           <div class="error" v-html="error" />
           <br>
           <v-btn
             dark
             class="cyan"
-            @click="register">
-            Register
+            @click="login">
+            Login
           </v-btn>
         </div>
       </div>
@@ -48,17 +43,14 @@ export default {
     }
   },
   methods: {
-    async register () {
+    async login () {
       try {
-        const response = await AuthenticationService.register({
+        const response = await AuthenticationService.login({
           email: this.email,
           password: this.password
         })
         this.$store.dispatch('setToken', response.data.token)
         this.$store.dispatch('setUser', response.data.user)
-        // this.$router.push({
-          // name: 'songs'
-        // })
       } catch (error) {
         this.error = error.response.data.error
       }
